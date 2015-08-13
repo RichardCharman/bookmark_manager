7ffc7294641c585d5edf8e5170d487f11e8a0e0a
Richard Charman <richardcharman@live.com>
2015-08-13 14:32:57 +0000

    Added Password Confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@
 require 'bcrypt' # make sure 'bcrypt' is in your Gemfile
 
 class User
+  attr_reader :password
+  attr_accessor :password_confirmation
 
   include DataMapper::Resource
 
@@ -20,7 +22,9 @@ class User
   # has both the password hash and the salt. We save it to the
   # database instead of the plain password for security reasons.
   def password=(password)
+    @password = password
     self.password_digest = BCrypt::Password.create(password)
   end
 
+  validates_confirmation_of :password
 end
